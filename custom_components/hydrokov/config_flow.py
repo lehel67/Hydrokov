ae86150f3d377120044864de611b54fb5f5a4cbe
lehel67 <48632086+lehel67@users.noreply.github.com>
2026-05-07 21:53:09 +0200

    Add files via upload

---
+++ custom_components/hydrokov/config_flow.py
@@ -0,0 +1,31 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from .const import DOMAIN, CONF_CLIENT_ID
+
+class HydrokovConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Hydrokov",
+                data={
+                    CONF_EMAIL: user_input[CONF_EMAIL],
+                    CONF_PASSWORD: user_input[CONF_PASSWORD],
+                    CONF_CLIENT_ID: user_input.get(CONF_CLIENT_ID, "TSY4647"),
+                }
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_EMAIL): str,
+                vol.Required(CONF_PASSWORD): str,
+                vol.Optional(CONF_CLIENT_ID): str,
+            }),
+            errors=errors,
+        )
---
+++ custom_components/hydrokov/const.py
@@ -0,0 +1,4 @@
+DOMAIN = "hydrokov"
+CONF_CLIENT_ID = "client_id"
+
+DEFAULT_SCAN_INTERVAL = 6 * 3600  # 6 óránként
